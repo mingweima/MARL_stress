9151b6168552fca3ef75c25a431aba7609becd50
Mingwei Ma <mingwei.ma@queens.ox.ac.uk>
2019-10-31 12:35:13 +0000

    Changed back to tanh, which is correct

--- Naive_MARL/NaiveA2C/ac_model.py
+++ Naive_MARL/NaiveA2C/ac_model.py
@@ -38,7 +38,7 @@ class Actor(nn.Module):
         """Build an actor (policy) network that maps states -> actions."""
         x = torch.relu(self.fc1(state))
         x = torch.relu(self.fc2(x))
-        return torch.sigmoid(self.fc3(x))
+        return torch.tanh(self.fc3(x))
 
 
 class Critic(nn.Module):
